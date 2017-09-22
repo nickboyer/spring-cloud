7f3afe89c1eb0c8f58b86e511717743d7085a693
nickboyer <nickboyer@foxmail.com>
2017-09-22 18:12:09 +0800

    commit

--- config-client/src/main/java/com/nickboyer/configclient/ConfigClientApplication.java
+++ config-client/src/main/java/com/nickboyer/configclient/ConfigClientApplication.java
@@ -1,5 +1,7 @@
 package com.nickboyer.configclient;
 
+import java.io.UnsupportedEncodingException;
+
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -16,14 +18,33 @@ public class ConfigClientApplication {
 		SpringApplication.run(ConfigClientApplication.class, args);
 	}
 
-	@Value("${name}")
 	String name;
+
+	/**
+	 * @return name
+	 */
+	public String getName() {
+		return name;
+	}
+
+	/**
+	 * @param name 要设置的 name
+	 */
+	@Value("${name}")
+	public void setName(String name) {
+		try {
+			this.name = new String(name.getBytes("ISO-8859-1"), "UTF-8");
+		} catch (UnsupportedEncodingException e) {
+			// 添加日志记录该异常
+			e.printStackTrace();
+		}
+	}
 
 	@Value("${id}")
 	String id;
 
 	@RequestMapping("/test")
-	public String test() {
+	public String test() throws UnsupportedEncodingException {
 
 		return "name=" + name + ",id=" + id;
 	}
