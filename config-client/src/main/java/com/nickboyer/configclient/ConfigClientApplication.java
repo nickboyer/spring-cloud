5dbc5a7e35e4f354439e15a5e4f8ebcf7f7caea9
nickboyer <nickboyer@foxmail.com>
2017-09-22 13:36:59 +0800

    commit

---
+++ config-client/src/main/java/com/nickboyer/configclient/ConfigClientApplication.java
@@ -0,0 +1,30 @@
+package com.nickboyer.configclient;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+@EnableEurekaClient
+public class ConfigClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConfigClientApplication.class, args);
+	}
+
+	@Value("${name}")
+	String name;
+
+	@Value("${id}")
+	String id;
+
+	@RequestMapping("/test")
+	public String test() {
+
+		return "name=" + name + ",id=" + id;
+	}
+}
--- eureka-client_2/src/main/java/com/nickobyer/eurekaclient_2/HelloService.java
+++ eureka-client_2/src/main/java/com/nickobyer/eurekaclient_2/HelloService.java
@@ -13,8 +13,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
-import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
-
 /**
  * @title
  * @description
@@ -26,7 +24,7 @@ public class HelloService {
 	@Autowired
 	private RestTemplate restTemplate;
 
-	@HystrixCommand(fallbackMethod = "hiError")
+	// @HystrixCommand(fallbackMethod = "hiError")
 	public String hiService(String name) {
 
 		return restTemplate.getForObject("http://eureka-client-1/test?name=" + name, String.class);
