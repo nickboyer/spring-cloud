be6ad18ce7f7201090e8e0d4ca2cdd373668c4e0
nickboyer <nickboyer@foxmail.com>
2017-09-22 17:43:49 +0800

    commit

--- config-client/src/test/java/com/nickboyer/configclient/ConfigClientApplicationTests.java
+++ config-client/src/test/java/com/nickboyer/configclient/ConfigClientApplicationTests.java
@@ -2,6 +2,7 @@ package com.nickboyer.configclient;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
@@ -9,8 +10,13 @@ import org.springframework.test.context.junit4.SpringRunner;
 @SpringBootTest
 public class ConfigClientApplicationTests {
 
+	@Value("${name}")
+	String name;
+
 	@Test
 	public void contextLoads() {
+
+		System.out.println(name);
 	}
 
 }
